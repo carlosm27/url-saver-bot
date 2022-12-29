627bf16627d77b1cc5d6406b16b3aa2ae61a7816
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2022-12-28 20:34:05 -0400

    Update data.rs

--- src/data.rs
+++ src/data.rs
@@ -1,8 +1,24 @@
 use once_cell::sync::Lazy;
 
 use std::sync::Mutex;
+use std::collections::HashMap;
 
-static DATA: Lazy<Mutex<HashMap<UUID, StoredURL>>> = Lazy::new(|| Mutex::new(
+use serde::{Deserialize, Serialize};
+//use serde_json::json;
+
+#[derive(Debug,Clone, Eq, Hash, PartialEq,Serialize,Deserialize)]
+pub struct StoredURL {
+    pub id: String,
+    pub https_address: String,
+}
+
+impl std::fmt::Display for StoredURL {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.https_address)
+    }
+}
+
+pub static DATA: Lazy<Mutex<HashMap<String, StoredURL>>> = Lazy::new(|| Mutex::new(
     HashMap::from([
         
     ])
